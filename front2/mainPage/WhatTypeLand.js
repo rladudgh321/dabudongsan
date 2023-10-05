b8a9fc7dcf9be1b7da314a6e04b62b467148fcd0
kyh <kyh_0625@naver.com>
2023-10-06 03:26:23 +0900

    0.0.3

--- front2/mainPage/WhatTypeLand.js
+++ front2/mainPage/WhatTypeLand.js
@@ -1,11 +1,28 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Card } from 'antd';
 import React from 'react';
 
 const WhatTypeLand = () => {
     return (
         <>
-            <Row>
-                <Col xs={8}></Col>
+            <Row gutter={10} style={{margin: '15px 5px'}}>
+                <Col xs={8}>
+                    <Card actions={['아파트']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/apart.png")', height:'115px' }} ></div>} />            
+                </Col>
+                <Col xs={8}>
+                    <Card actions={['신축빌라']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/villa.png")', height:'115px' }} ></div>} />            
+                </Col>
+                <Col xs={8}>
+                    <Card actions={['원/투/쓰리룸']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/oneroom.png")', height:'115px' }} ></div>} />            
+                </Col>
+                <Col xs={8}>
+                    <Card actions={['사무실']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/office.png")', height:'115px' }} ></div>} />            
+                </Col>
+                <Col xs={8}>
+                    <Card actions={['상가']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/store.png")', height:'115px' }} ></div>} />            
+                </Col>
+                <Col xs={8}>
+                    <Card actions={['오피스텔']} bodyStyle={{display:'none'}} cover={<div style={{ backgroundImage:'url("/img/1/officetel.png")', height:'115px' }} ></div>} />            
+                </Col>
             </Row>
         </>
     );
--- front2/pages/index.js
+++ front2/pages/index.js
@@ -3,6 +3,8 @@ import AppLayout from '../components/AppLayout';
 import MainPicture from '../mainPage/mainPicture';
 import SearchMapList from '../mainPage/SearchMapList';
 import WhatTypeLand from '../mainPage/WhatTypeLand';
+import IfLandType from '../mainPage/IfLandType';
+import RecommedLand from '../mainPage/RecommedLand';
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
         <MainPicture />
         <SearchMapList />
         <WhatTypeLand />
+        <IfLandType />
+        <RecommedLand />
       </AppLayout>
     </>
   );
