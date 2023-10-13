7afde4747cba8b905d9edcfaaf10d24d6ed8a3b3
rladudgh321 <kyh_0625@naver.com>
2023-10-13 18:29:22 +0900

    0.2.3. contact reducer

--- front2/components/Map.js
+++ front2/components/Map.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Map = () => {
-    return (
-        <>
-            Map
-        </>
-    );
-}
+const KakaoMap = () => {
+  useEffect(() => {
+    // Kakao Map API 스크립트를 동적으로 로드
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=221865f87f3f5b0bb2ac662f197e31e1`;
+    script.async = true;
+    script.onload = () => {
+      // Kakao Map API 스크립트 로드 완료 후 실행될 코드
+      const mapContainer = document.getElementById('map');
+          const options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+          };
+      const map = new window.kakao.maps.Map(mapContainer, options);
+    };
+    document.head.appendChild(script);
+  }, []);
 
-export default Map;
+  return (
+    <div>
+      {/* 지도를 표시할 div */}
+      <div id="map" style={{ width: '100%', height: '350px' }}></div>
+    </div>
+  );
+};
+
+export default KakaoMap;
--- front2/pages/_document.js
+++ front2/pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+      </Head>
       <body>
         <Main />
         <NextScript />
