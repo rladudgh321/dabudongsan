373b3a2eba2ef608bb488fe6d47426ddcee8b7bf
rladudgh321 <kyh_0625@naver.com>
2023-10-14 23:26:56 +0900

    Updates

--- back/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isNotLoggedIn } = require('../middleware');
-const { login } = require('../controllers/user');
-
-router.post('/', isNotLoggedIn, login);
-
-module.exports = router;
--- front2/config/config.js
+++
@@ -1,6 +0,0 @@
-export const prefix =
-  process.env.NODE_ENV === "production"
-    ? "https://rladudgh321.github.io/dabudongsan"
-    : "";
-
-    
